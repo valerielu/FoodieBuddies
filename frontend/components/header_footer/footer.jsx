35d025e18b9b4fbfae824e83052413be4e9c77dc
valerielu <valerie.huanchen.lu@gmail.com>
2016-09-14 18:07:20 -0700

    added footer

--- frontend/components/header_footer/footer.jsx
+++ frontend/components/header_footer/footer.jsx
@@ -3,6 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
+      <a className="icon-links" href="https://github.com/valerielu/FoodieBuddies" target="_blank"><i className="fa fa-github" aria-hidden="true"></i></a>
+      <a className="icon-links" href="https://www.linkedin.com/in/valerie-lu" target="_blank"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
       <div className="footer-quote">Stay Hungry. Stay Foolish.</div>
     </footer>
   );
